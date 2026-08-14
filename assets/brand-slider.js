6e94fc0ed89b062dc05936df0fba088536cd1ec2
Thom Bennett <thom@pswebsitedesign.com>
2026-08-14 12:27:16 +0100

    New sections

--- assets/brand-slider.js
+++ assets/brand-slider.js
@@ -4,7 +4,6 @@ class BrandSliderComponent extends HTMLElement {
     this._brandSliderReady = true;
 
     this.slider = this.querySelector('[id^="Slider-"]');
-    this.sliderItems = this.querySelectorAll('[id^="Slide-"]');
     this.currentPageElement = this.querySelector('.slider-counter--current');
     this.pageTotalElement = this.querySelector('.slider-counter--total');
     this.prevButton = this.querySelector('button[name="previous"]');
@@ -16,33 +15,88 @@ class BrandSliderComponent extends HTMLElement {
     const resizeObserver = new ResizeObserver(() => this.initPages());
     resizeObserver.observe(this.slider);
 
-    this.slider.addEventListener('scroll', () => this.update());
+    this.slider.addEventListener('scroll', () => this.onScroll(), { passive: true });
     this.prevButton.addEventListener('click', (event) => this.onButtonClick(event));
     this.nextButton.addEventListener('click', (event) => this.onButtonClick(event));
   }
 
+  getOriginalSlides() {
+    return Array.from(this.slider.querySelectorAll('[id^="Slide-"]')).filter(
+      (element) => !element.hasAttribute('data-brand-slider-clone')
+    );
+  }
+
+  removeClones() {
+    this.slider.querySelectorAll('[data-brand-slider-clone]').forEach((clone) => clone.remove());
+  }
+
+  appendClones() {
+    const originals = this.getOriginalSlides().filter((element) => element.clientWidth > 0);
+    if (originals.length <= this.slidesPerPage) return;
+
+    const cloneCount = Math.min(this.slidesPerPage, originals.length);
+    for (let i = 0; i < cloneCount; i++) {
+      const clone = originals[i].cloneNode(true);
+      clone.removeAttribute('id');
+      clone.removeAttribute('data-shopify-editor-block');
+      clone.setAttribute('data-brand-slider-clone', '');
+      clone.setAttribute('aria-hidden', 'true');
+      clone.classList.add('brand-slider__item--clone');
+      this.slider.appendChild(clone);
+    }
+  }
+
   initPages() {
-    this.sliderItems = this.querySelectorAll('[id^="Slide-"]');
-    this.sliderItemsToShow = Array.from(this.sliderItems).filter((element) => element.clientWidth > 0);
+    const previousScrollLeft = this.slider.scrollLeft;
+    this.removeClones();
+    this.sliderItemsToShow = this.getOriginalSlides().filter((element) => element.clientWidth > 0);
     if (this.sliderItemsToShow.length < 2) return;
 
     this.sliderItemOffset = this.sliderItemsToShow[1].offsetLeft - this.sliderItemsToShow[0].offsetLeft;
     this.slidesPerPage = Math.floor(this.slider.clientWidth / this.sliderItemOffset) || 1;
-    this.totalPages = Math.max(1, this.sliderItemsToShow.length - this.slidesPerPage + 1);
+    this.originalCount = this.sliderItemsToShow.length;
+    this.totalPages = Math.max(1, this.originalCount - this.slidesPerPage + 1);
+
+    if (this.originalCount > this.slidesPerPage) {
+      this.appendClones();
+      const firstClone = this.slider.querySelector('[data-brand-slider-clone]');
+      this.loopScrollWidth = firstClone ? firstClone.offsetLeft : 0;
+    } else {
+      this.loopScrollWidth = 0;
+    }
+
+    this.slider.scrollLeft = Math.min(previousScrollLeft, this.getMaxRealScrollLeft());
     this.update();
   }
 
-  getMaxScrollLeft() {
-    return Math.max(0, this.slider.scrollWidth - this.slider.clientWidth);
+  getMaxRealScrollLeft() {
+    const lastOriginal = this.sliderItemsToShow[this.originalCount - 1];
+    if (!lastOriginal) return 0;
+    return Math.max(0, lastOriginal.offsetLeft + lastOriginal.clientWidth - this.slider.clientWidth);
+  }
+
+  normalizeScrollPosition() {
+    if (!this.loopScrollWidth || this._scrollLoopLock) return;
+
+    if (this.slider.scrollLeft >= this.loopScrollWidth - 1) {
+      this._scrollLoopLock = true;
+      this.slider.scrollLeft = this.slider.scrollLeft - this.loopScrollWidth;
+      this._scrollLoopLock = false;
+    }
+  }
+
+  onScroll() {
+    this.normalizeScrollPosition();
+    this.update();
   }
 
   update() {
     if (!this.slider || !this.nextButton) return;
 
-    const maxScrollLeft = this.getMaxScrollLeft();
+    const maxRealScrollLeft = this.getMaxRealScrollLeft();
     const previousPage = this.currentPage;
 
-    if (maxScrollLeft > 0 && this.slider.scrollLeft >= maxScrollLeft - 1) {
+    if (maxRealScrollLeft > 0 && this.slider.scrollLeft >= maxRealScrollLeft - 1) {
       this.currentPage = this.totalPages;
     } else {
       this.currentPage = Math.round(this.slider.scrollLeft / this.sliderItemOffset) + 1;
@@ -72,16 +126,18 @@ class BrandSliderComponent extends HTMLElement {
 
   onButtonClick(event) {
     event.preventDefault();
-    const maxScrollLeft = this.getMaxScrollLeft();
+    if (!this.sliderItemOffset) return;
+
+    const maxRealScrollLeft = this.getMaxRealScrollLeft();
 
     if (event.currentTarget.name === 'next') {
-      if (this.currentPage >= this.totalPages) {
-        this.slideScrollPosition = 0;
+      if (this.slider.scrollLeft >= maxRealScrollLeft - 1) {
+        this.slideScrollPosition = this.slider.scrollLeft + this.sliderItemOffset;
       } else {
-        this.slideScrollPosition = Math.min(this.slider.scrollLeft + this.sliderItemOffset, maxScrollLeft);
+        this.slideScrollPosition = Math.min(this.slider.scrollLeft + this.sliderItemOffset, maxRealScrollLeft);
       }
-    } else if (this.currentPage <= 1) {
-      this.slideScrollPosition = maxScrollLeft;
+    } else if (this.slider.scrollLeft <= 1) {
+      this.slideScrollPosition = maxRealScrollLeft;
     } else {
       this.slideScrollPosition = Math.max(this.slider.scrollLeft - this.sliderItemOffset, 0);
     }
